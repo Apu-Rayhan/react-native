405e6507b9a99a1540819b2e30eeec02fc286ae0
opu-rayhan <apuhot168@gmail.com>
2022-12-27 03:46:22 +0600

    hhhh

--- App.js
+++ App.js
@@ -1,42 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,TouchableOpacity, Text, View , ImageBackground, ImageBackgroundComponent } from 'react-native';
-import LiginTextInput from './component/Login';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import LiginTextInput from "./component/Login";
 
 export default function App() {
   return (
-    <View style={styles.container} source={{uri:'https://narayanganjclubltd.com/images/new/NCL.jpg'} }>
-    <ImageBackground   style={styles.image}>
-    
-    <LiginTextInput/>
-    <Text>Open first react native appp</Text>
-    <TouchableOpacity
-        onPress={() => alert('Hello, world!')}
-        style={{ backgroundColor: 'blue' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
-      </TouchableOpacity>
-    <StatusBar style="auto" />
-    </ImageBackground>
+    <View style={styles.container}>
+      <LiginTextInput />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor: `#b0c4de`,
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-    
+    flex: 1,
+    backgroundColor: `#C0C0C0`,
+    justifyContent: "center",
   },
-  image:{
-  
-    justifyContent:'center',
-    width:350,
-    height:600,
-    // opacity:0.2,
-    
-    
-  },
-  
 });
--- component/Login.jsx
+++ component/Login.jsx
@@ -1,38 +1,74 @@
 import React from "react";
-import { TextInput, StyleSheet, Text } from "react-native";
+import {
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 
 const LiginTextInput = () => {
   const [text, setText] = React.useState("");
   const [number, setNumber] = React.useState(null);
 
+  const handleInputText = ()=>{
+    alert(text)
+  }
+  
+
   return (
-    <>
-      <Text style={styles.text}>Narayanganj Club Ltd</Text>
-      <TextInput style={styles.input} onChangeText={setText} value={text} />
+    <View style={{ borderWidth: 1, margin: 5 , borderRadius:5 }}>
+      <Text style={styles.text}>NCL</Text>
       <TextInput
         style={styles.input}
-        onChangeText={setNumber}
+        onChangeText={e=>setText(e.target.value)}
+        placeholder="Email"
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={e=>setNumber(e.target.value)}
         value={number}
         placeholder="Password"
         keyboardType="numeric"
       />
-    </>
+      <View style={{ marginLeft: 120, width: 120 }}>
+        <TouchableOpacity style={styles.btn} onPress={handleInputText} >
+          <Text style={styles.Tlog}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: "#00FF00",
+    width: 120,
+    height: 40,
+    borderRadius: 20,
+  },
+  Tlog: {
+    fontWeight: "bold",
+    fontSize: 20,
+    alignContent: "center",
+    paddingLeft: 33,
+    paddingTop:5
+  },
   input: {
     height: 40,
-    margin: 12,
-    borderWidth: 1,
+    width:250,
+    marginLeft:60,
+    marginBottom:10,
+    borderBottomWidth:2,
     padding: 10,
   },
   text: {
     color: "blue",
-    fontWeight:'bold',
+    fontWeight: "bold",
     fontSize: 28,
     alignContent: "center",
-    paddingLeft: 30,
+    paddingLeft: 150,
   },
 });
 
